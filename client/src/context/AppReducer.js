e0dd0c40ed33cae9d7425a2bee64f225d7607462
binothird <albinoiii.cordova@gmail.com>
2020-11-10 23:53:00 +0800

    Updated action type as constant

--- client/src/context/AppReducer.js
+++ client/src/context/AppReducer.js
@@ -1,6 +1,8 @@
+import * as actions from "../util/actionTypes";
+
 export default (state, action) => {
   switch (action.type) {
-    case "SUCCESS_LOGIN":
+    case actions.SUCCESS_LOGIN:
       return {
         ...state,
         credentials: {
@@ -13,14 +15,14 @@ export default (state, action) => {
         loadingStatuses: { ...state.loadingStatuses, logIn: false },
       };
 
-    case "ERROR_LOGIN":
+    case actions.ERROR_LOGIN:
       return {
         ...state,
         errorMessage: action.payload.message,
         loadingStatuses: { ...state.loadingStatuses, logIn: false },
       };
 
-    case "LOGOUT_USER":
+    case actions.LOGOUT_USER:
       return {
         ...state,
         token: null,
@@ -30,7 +32,7 @@ export default (state, action) => {
         loadingStatuses: { ...state.loadingStatuses, get: true },
       };
 
-    case "SUCCESS_REGISTER":
+    case actions.SUCCESS_REGISTER:
       return {
         ...state,
         credentials: {
@@ -43,58 +45,58 @@ export default (state, action) => {
         loadingStatuses: { ...state.loadingStatuses, register: false },
       };
 
-    case "ERROR_REGISTER":
+    case actions.ERROR_REGISTER:
       return {
         ...state,
         errorMessage: action.payload,
         loadingStatuses: { ...state.loadingStatuses, register: false },
       };
 
-    case "CLEAR_ERRORMESSAGE":
+    case actions.CLEAR_ERRORMESSAGE:
       return {
         ...state,
         errorMessage: "",
       };
 
-    case "GET_TODOS":
+    case actions.GET_TODOS:
       return {
         ...state,
         loadingStatuses: { ...state.loadingStatuses, get: false },
         todos: action.payload,
       };
 
-    case "LOAD_LOCALCREDENTIALS":
+    case actions.LOAD_LOCALCREDENTIALS:
       return {
         ...state,
         credentials: action.payload,
       };
 
-    case "ADD_TODO":
+    case actions.ADD_TODO:
       return {
         ...state,
         loadingStatuses: { ...state.loadingStatuses, add: false },
         todos: [...state.todos, action.payload],
       };
 
-    case "COMPLETE_TODO":
+    case actions.COMPLETE_TODO:
       return {
         ...state,
         loadingStatuses: { ...state.loadingStatuses, complete: false },
         todos: state.todos.filter(({ _id }) => _id !== action.payload),
       };
 
-    case "EDIT_TODO":
+    case actions.EDIT_TODO:
       const todo = state.todos.find(({ _id }) => _id === action.payload._id);
       todo.todo = action.payload.todo;
       return { ...state };
 
-    case "SET_LOADING":
+    case actions.SET_LOADING:
       return {
         ...state,
         loadingStatuses: { ...state.loadingStatuses, ...action.payload },
       };
 
-    case "ERROR_TODO":
+    case actions.ERROR_TODO:
       return {
         ...state,
         error: action.payload,
